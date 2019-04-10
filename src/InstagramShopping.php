267eb71b9f9000b585bb59b646862f14802192fa
Alexander Wolf <alexander@goltfisch.de>
2019-04-10 17:18:46 +0200

    Initial commit

---
+++ src/InstagramShopping.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace SalesChannelTeam\Plugin\InstagramShopping;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+
+class InstagramShopping extends Plugin
+{
+    
+}
