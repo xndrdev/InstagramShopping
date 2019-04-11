61b1abb4c9568af1326a2d1ab61ea7d0c392b272
Sebastian Ruchlewicz <sebastian.ruchlewicz@gmail.com>
2019-04-11 15:47:45 +0200

    ajax call to /instagram-shopping-export for instagram shopping module

--- src/Administration/module/instagram-shopping/page/instagram-shopping-settings/index.js
+++ src/Administration/module/instagram-shopping/page/instagram-shopping-settings/index.js
@@ -1,6 +1,19 @@
-import { Component } from 'src/core/shopware';
+import { Application, Component } from 'src/core/shopware';
 import template from './instagram-shopping-settings.html.twig';
+//import HttpClient from 'asset/script/service/http-client.service';
 
 Component.register('instagram-shopping-settings', {
-    template
+    template,
+
+    methods: {
+        exportProducts() {
+            const httpClient = Application.getContainer('init').httpClient;
+
+            httpClient.post(
+                '/instagram-shopping-export'
+            ).then((response) => {
+                console.log(response.data);
+            });
+        },
+    }
 });
