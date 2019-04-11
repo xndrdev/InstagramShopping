aec8850ea3b874317c06b3c83c0b367bb245aa63
Bastian Kurz <bastian.kurz@brickfox.de>
2019-04-11 09:42:35 +0200

    first branch push

---
+++ Controller/InstagramShoppingActionController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * InstagramShoppingActionController.php
+ * This file is part of brickfox.
+ *
+ * @author    brickfox GmbH <support@brickfox.de>
+ * @copyright Copyright (c) 2011-2019 brickfox GmbH http://www.brickfox.de
+ */
+
+namespace InstagramShopping\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class InstagramShoppingActionController extends AbstractController
+{
+
+}
